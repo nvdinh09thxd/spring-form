24be8fbc49edcca72eb28b89c2d20d4b64ce278f
nvdinh09thxd <nvdinh09thxd@gmail.com>
2021-11-11 09:06:15 +0700

    edit sth

--- src/main/java/edu/vinaenter/controllers/EmployeeController.java
+++ src/main/java/edu/vinaenter/controllers/EmployeeController.java
@@ -2,8 +2,8 @@ package edu.vinaenter.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,14 +14,14 @@ public class EmployeeController {
 
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
 	public String addEmployee(Model model) {
-		model.addAttribute("employee", new Employee());
+		model.addAttribute("employee", new Employee(1, "Nguyen Van Dinh"));
 		return "add-employee";
 	}
 
-	@RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
-	public String doAddEmployee(@ModelAttribute("employee") Employee employee, ModelMap modelMap) {
+	@PostMapping("/addEmployee")
+	public String doAddEmployee(@ModelAttribute Employee employee, Model model) {
 		employee.setName(employee.getName().toUpperCase());
-		modelMap.addAttribute("employee", employee);
+		model.addAttribute("employee", employee);
 		return "view-employee";
 	}
 
